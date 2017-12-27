4b0d0334293a58be4867745e2e52d4b74f7ae980
Michael Kilchenmann <zuppi@Michaels-MacBook.local>
2017-12-27 07:59:51 +0100

    lupd

--- P_Python/dt_wiki_aggr.py
+++ P_Python/dt_wiki_aggr.py
@@ -35,13 +35,17 @@ def main():
         f.close()
 
     # set up empty pandas dataframe to append the single pickles into
-    dt_pd_wiki = pd.DataFrame(columns=['project', 'page_title', 'counter', 'rsize', 'tstamp'])
-    dt_pd_wiki.set_index('page_title', inplace=True, drop=True, append=False, verify_integrity=True)
+    dt_pd_wiki_pk = pd.DataFrame(columns=['project', 'page_title', 'counter', 'rsize', 'tstamp'])
+    dt_pd_wiki_pk.set_index('page_title', inplace=True, drop=True, append=False, verify_integrity=True)
 
-    for dt_pickle in dt_pickles:
-        print('dt_pickle:', dt_pickle)
-        dt_pd_wiki = dt_pd_wiki.append(pd.read_pickle(dt_pickle))
+    for dt_pickle_pk in dt_pickles:
+        print('dt_pickle:', dt_pickle_pk)
+        dt_pd_wiki_pk = dt_pd_wiki_pk.append(pd.read_pickle(dt_pickle_pk))
 
+    dt_pd_wiki_pk = dt_pd_wiki_pk[dt_pd_wiki_pk.project=='en']
+    dt_pd_wiki_pk = dt_pd_wiki_pk.drop('rsize', 1)
+    dt_pd_wiki_pk = dt_pd_wiki_pk.drop('project', 1)
+    dt_pd_wiki = dt_pd_wiki_pk
 
     # 2) Open CSV dump and append to pandas dataframe (source: https://tools.wmflabs.org/pageviews/)
     # Name of Data File (WIKIPEDIA)
@@ -56,8 +60,6 @@ def main():
     dt_pd_wiki_csv['page'] = 'Bitcoin'
     # verify_integrity = False as index will have duplicates, which is expected
     dt_pd_wiki_csv.set_index('page', inplace=True, drop=True, append=False, verify_integrity=False)
-    dt_pd_wiki_csv['project'] = 'en'
-    dt_pd_wiki_csv['rsize'] = 0
 
     # append dt_pd_wiki_csv to dt_pd_wiki
     dt_pd_wiki = dt_pd_wiki.append(dt_pd_wiki_csv)
@@ -86,10 +88,10 @@ def main():
 
     dt_pd_wiki = dt_pd_wiki.append(dt_pd_wiki_grep)
 
+
+    # aggreagation
     dt_pd_wiki.sort_values('tstamp', ascending=True)
     dt_pd_wiki = dt_pd_wiki.groupby(dt_pd_wiki.tstamp).first()
-    dt_pd_wiki = dt_pd_wiki.drop('project', 1)
-    dt_pd_wiki = dt_pd_wiki.drop('rsize', 1)
     dt_pd_wiki = dt_pd_wiki.astype(float)
     dt_pd_wiki = dt_pd_wiki.resample('D').sum()
 
@@ -102,6 +104,8 @@ def main():
     # Store pickle to disk
     os.chdir('..' + sl + '..' + sl + '..' + sl + 'P_Python' + sl)
     dt_pd_wiki.to_pickle('dt_pd_wiki.pickle')
+
+    dt_pd_wiki.plot()
 
     print(os.path.basename(__file__), 'executed')
 
